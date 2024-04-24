76d70e044d3217046b19b4739b990e2ce15c012b
Giresse <giresseachouakong@gmail.com>
2024-04-24 08:21:03 -0400

    Setting up site for a hair styling business

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/listings')
+def listings():
+    return render_template('listings.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/appointments')
+def appointments():
+    return render_template('appointments.html')
+
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
